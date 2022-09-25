986628b6f29f0a664598891fe2b2eddf966fb6f2
Eshanth1234 <88104266+Eshanth1234@users.noreply.github.com>
2022-09-24 17:43:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+moustacheX=0
+moustacheY=0
+
+function preload(){
+    moustache= loadImage('https://i.postimg.cc/bJSDtvhp/m-1.png');
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPose);
+}
+
+
+function take_snapshot(){
+    save("my_picture.png");
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotPose(results){
+    if(results.length > 0){
+        console.log(results);
+        moustacheX= results[0].pose.nose.x-15;
+        moustacheY= results[0].pose.nose.y+8;
+        console.log("nose x: "+moustacheX);
+        console.log("nose y: "+moustacheY);
+    };
+}
+function draw(){
+    image(video, 0 ,0 ,300,300);
+    image(moustache, moustacheX, moustacheY, 30 ,30 );
+}
